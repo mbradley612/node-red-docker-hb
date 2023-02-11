251102953d85a6e81de440208455238df0841792
Matthew Bradley <mbradley612@gmail.com>
2023-02-11 22:16:26 +0000

    Mods for rpi

--- docker-custom/docker-alpine.sh
+++ docker-custom/docker-alpine.sh
@@ -6,11 +6,11 @@ echo "node-red version: ${NODE_RED_VERSION}"
 echo "#########################################################################"
 
 docker build --rm --no-cache \
-    --build-arg ARCH=amd64 \
+    --build-arg ARCH=arm64v8 \
     --build-arg NODE_VERSION=16 \
     --build-arg NODE_RED_VERSION=${NODE_RED_VERSION} \
     --build-arg OS=alpine \
     --build-arg BUILD_DATE="$(date +"%Y-%m-%dT%H:%M:%SZ")" \
     --build-arg TAG_SUFFIX=default \
     --file Dockerfile.custom \
-    --tag testing:node-red-build .
+    --tag testing:node-red-mongodb-arm64v8 .
